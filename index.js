95166e32a54cde0d2d7fd14f782f86ca817b58f2
zyphorixx <24256@iiitu.ac.in>
2026-01-10 20:10:32 +0530

    Enforce auth validation in API Gateway for BookingService routes

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
-
-app.use(morgan('combined'));
-
 const PORT = 3005;
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 50
+});
+
+app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice', async (req, res, next) => {
+    // console.log(req.headers['x-access-token']);
+    const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated',{
+        headers : {
+            'x-access-token' : req.headers['x-access-token']
+        }
+    });
+    console.log(response.data);
+    next();
+});
+
+app.use('/bookingservice',createProxyMiddleware({target: 'http://localhost:3002',changeOrigin: true,pathRewrite: {  
+    '^/bookingservice': ''   
+}
+}));
+
 app.get('/home', (req, res) => {
-    return res.json({message : 'OK'});
+  res.json({ status: 'API Gateway is running' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started at PORT ${PORT}`);
+  console.log(`API Gateway running on PORT ${PORT}`);
 });
